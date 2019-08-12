bc74dbcd543b0c0d9f7197b65e870f98a7501458
zhangshixin <zhangshixin@yaomaiche.com>
2019-08-12 23:35:58 +0800

    注册逻辑完成，比较简单，注意 sql 语句的规则，多一个逗号也不行；还有 Controller 路径里多的 /user/xxx 是怎么来的，暂时不理解

--- fruit/src/main/java/com/fruitsalesplatform/controller/UserController.java
+++ fruit/src/main/java/com/fruitsalesplatform/controller/UserController.java
@@ -11,9 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 /**
  * Created by zhangshixin on 19/8/7.
@@ -49,6 +48,33 @@ public class UserController extends BaseController{
         //带着错误信息回到登录页
         model.addAttribute("errorMsg", "登录失败！账号或密码错误！");
         model.addAttribute("noticeMsg", "输入错误次数太多将导致封号！");
+        return "/login.jsp";
+    }
+
+    @GetMapping("/user/registerPage.action")
+    public String toRegister() {
+        //转向注册页面
+        return "/register.jsp";
+    }
+
+    @PostMapping("/user/register.action")
+    public String register(User user, Model model, HttpServletRequest request,
+                           HttpServletResponse response) {
+        //1.先去查一下用户名是否被使用
+        Map<String, String> params = new HashMap<>();
+        params.put("username", user.getUsername());
+
+        List<User> users = userService.find(params);
+        if (users != null && users.size() > 0) {
+            //已被注册侧，转发回注册页面
+            model.addAttribute("errorMsg", "注册失败，用户名已被占用");
+            return "/register.jsp";
+        }
+
+        //2.未注册，设置 uid 保存到数据库
+        user.setUserid(UUID.randomUUID().toString());
+        userService.insert(user);
+        model.addAttribute("noticeMsg", "注册成功，请输入账号密码登录");
         return "/login.jsp";
     }
 }
